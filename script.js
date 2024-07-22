cf3c2206339d99ff349e873440014f3aeadea340
Monyc <158827494+MonycLAC@users.noreply.github.com>
2024-07-22 13:09:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+let musicas = [
+    {
+        titulo: 'CORAÇÃO PARTIDO - ELA JOGA NO 7',
+        artista: 'Netto Britto',
+        src: 'Musicas/11 - CORAÇÃO PARTIDO - ELA JOGA NO 7.mp3',
+        img: 'Imagens/andrew-varnum-nbdVeCABFeU-unsplash.jpg'
+    },
+    {
+        titulo: 'VOCÊ NÃO VER',
+        artista: 'Netto Britto',
+        src: 'Musicas/02 - VOCÊ NÃO VÊ - LÁBIOS DIVIDIDOS.mp3',
+        img: 'Imagens/kenny-gaines-U-d828ayEdw-unsplash.jpg'
+    },
+    {
+        titulo: 'DEPOIS DE MIM',
+        artista: 'Netto Britto',
+        src: 'Musicas/12 - DEPOIS DE MIM.mp3',
+        img: 'Imagens/kenny-gaines-YScLVuOv7nU-unsplash.jpg'
+    }
+];
+
+let musica = document.querySelector('audio');
+let indexMusica = 0;
+
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
+
+// Inicializar a primeira música
+renderizarMusica(indexMusica);
+
+// Eventos
+document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
+musica.addEventListener('timeupdate', atualizarBarra);
+musica.addEventListener('ended', proximaMusica);
+
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if (indexMusica < 0) {
+        indexMusica = musicas.length - 1;
+    }
+    renderizarMusica(indexMusica);
+});
+
+document.querySelector('.proxima').addEventListener('click', proximaMusica);
+
+// Funções
+function renderizarMusica(index) {
+    console.log("Renderizando música:", musicas[index]);
+    
+    nomeMusica.textContent = musicas[index].titulo;
+    nomeArtista.textContent = musicas[index].artista;
+    imagem.src = musicas[index].img;
+    musica.src = musicas[index].src;
+    duracaoMusica.textContent = '0:00';
+
+    // Adicionar evento 'loadedmetadata' para atualizar a duração
+    musica.addEventListener('loadedmetadata', () => {
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}
+
+function tocarMusica() {
+    console.log("Tocando música:", musicas[indexMusica]);
+    musica.play();
+    document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
+}
+
+function pausarMusica() {
+    console.log("Pausando música:", musicas[indexMusica]);
+    musica.pause();
+    document.querySelector('.botao-pause').style.display = 'none';
+    document.querySelector('.botao-play').style.display = 'block';
+}
+
+function atualizarBarra(){
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+    let tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
+}
+
+function segundosParaMinutos(segundos) {
+    let minutos = Math.floor(segundos / 60);
+    segundos = segundos % 60;
+    if (segundos < 10) {
+        segundos = "0" + segundos;
+    }
+    return minutos + ":" + segundos;
+}
+
+function proximaMusica() {
+    indexMusica++;
+    if (indexMusica >= musicas.length) {
+        indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+    tocarMusica();
+}
+
+// Inicializar a primeira música
+renderizarMusica(indexMusica);
