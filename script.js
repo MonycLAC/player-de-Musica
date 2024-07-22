0ef1aba12557adb407f8b2edeb589840e02e0e97
Monyc <158827494+MonycLAC@users.noreply.github.com>
2024-07-22 13:17:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ let musicas = [
     {
         titulo: 'CORAÇÃO PARTIDO - ELA JOGA NO 7',
         artista: 'Netto Britto',
-        src: 'https://github.com/MonycLAC/player-de-Musica/blob/main/11%20-%20CORA%C3%87%C3%83O%20PARTIDO%20-%20ELA%20JOGA%20NO%207.mp3',
+        src: 'Musicas/11 - CORAÇÃO PARTIDO - ELA JOGA NO 7.mp3',
         img: 'Imagens/andrew-varnum-nbdVeCABFeU-unsplash.jpg'
     },
     {
         titulo: 'VOCÊ NÃO VER',
         artista: 'Netto Britto',
-        src: 'https://github.com/MonycLAC/player-de-Musica/blob/main/02%20-%20VOC%C3%8A%20N%C3%83O%20V%C3%8A%20-%20L%C3%81BIOS%20DIVIDIDOS.mp3',
+        src: 'Musicas/02 - VOCÊ NÃO VÊ - LÁBIOS DIVIDIDOS.mp3',
         img: 'Imagens/kenny-gaines-U-d828ayEdw-unsplash.jpg'
     },
     {
         titulo: 'DEPOIS DE MIM',
         artista: 'Netto Britto',
-        src: 'https://github.com/MonycLAC/player-de-Musica/blob/main/12%20-%20DEPOIS%20DE%20MIM.mp3 ',
+        src: 'Musicas/12 - DEPOIS DE MIM.mp3',
         img: 'Imagens/kenny-gaines-YScLVuOv7nU-unsplash.jpg'
     }
 ];
